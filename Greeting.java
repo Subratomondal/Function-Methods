2d66995b50bb69f76112c54fd2b0822ca7e2a6fa
Subrato Mondal <subratomondal374@gmail.com>
2024-09-25 22:16:01 +0530

    learning about functions/methods is java

---
+++ Greeting.java
@@ -0,0 +1,8 @@
+public class Greeting {
+    public static void main(String[] args) {
+        display();
+    }
+    static void display(){
+        System.out.println("Hello everyone am learning java");
+    }
+}
