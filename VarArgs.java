13d291ade2a1f461efd1fd0c7b0db43661dd0720
Subrato Mondal <subratomondal374@gmail.com>
2024-09-28 13:54:36 +0530

    learning about Variable argumnets

---
+++ VarArgs.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class VarArgs {
+    public static void main(String[] args) {
+        //fun(1,2,3,4,5,6,7,8);
+        multiple(2,4,"RAM","SHYAM","SUBRATO");
+    }
+
+    static void multiple(int a, int b, String ...v){
+
+    }
+    static void fun(int ...v){
+        System.out.println(Arrays.toString(v));
+    }
+}
